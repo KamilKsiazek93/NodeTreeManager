9b7e3ed98351035e586c11cbc34e191a09834542
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-02-05 12:29:34 +0100

    Added dbContext

---
+++ Models/NodeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NodesTreeManager.Models
+{
+    public class NodeDbContext : DbContext
+    {
+        public DbSet<Node> Nodes { get; set; }
+        public NodeDbContext(DbContextOptions<NodeDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
