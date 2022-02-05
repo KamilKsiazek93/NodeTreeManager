e18b4c2755b0335d9b63417c7661d1829126867a
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-02-05 12:19:36 +0100

    Added basic model and model for getting list of child

---
+++ Models/NodeTree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NodesTreeManager.Models
+{
+    public class NodeTree : Node
+    {
+        public List<Node> NodesChild { get; set; }
+    }
+}
